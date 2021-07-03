6426bc6240afb7135bac8bade33b77df60435786
jisu Yoon <jisu91113@gmail.com>
2021-07-03 18:31:49 +0900

    login-form

--- client/src/App.js
+++ client/src/App.js
@@ -4,11 +4,6 @@ import Login from './routes/Login';
 import Home from './routes/Home';
 import './App.css';
 
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons'
-
-library.add(faCheckSquare, faCoffee)
-
 function App() {
   return (
     <BrowserRouter>
