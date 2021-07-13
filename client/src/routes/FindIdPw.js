9093f4dc1119838c669de4ee1bf103de0323c7f8
jisu Yoon <jisu91113@gmail.com>
2021-07-13 23:30:01 +0900

    RightTitle

---
+++ client/src/routes/FindIdPw.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+//navbar밑에 오게 하려면?
+const FindIdPw = () => {
+  return <span>this is FindIdPw</span>;
+}
+
+export default FindIdPw;
---
+++ client/src/routes/Join.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+//navbar밑에 오게 하려면?
+const Join = () => {
+  return <span>this is Join</span>;
+}
+
+export default Join;
