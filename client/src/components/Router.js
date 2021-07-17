5bcaf503b2a8482879c54e8b5dbcc2cc871f94e2
jisu Yoon <jisu91113@gmail.com>
2021-07-18 00:31:20 +0900

    integrate Login

--- client/src/components/Router.js
+++ client/src/components/Router.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Navigationbar from './Navbar';
 import About from '../routes/About';
 import Login from '../routes/Login';
 import Home from '../routes/Home';
@@ -8,9 +7,7 @@ import Home from '../routes/Home';
 // Components/Routes.js 로 이동
 export default () => (
     <>
-    {/* <Navigationbar /> */}
     <Router>
-        <Navigationbar />
         <Route path="/" exact={true} component={Home} />
         <Route path="/about" component={About} />
         <Route path="/login" component={Login} />
