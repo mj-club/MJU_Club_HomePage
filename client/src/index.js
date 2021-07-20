54213b43a4135b659a2bc00f95202eedaa5794d2
dj-1087 <sam1087@mju.ac.kr>
2021-07-20 19:49:18 +0900

    for deploy

--- client/src/index.js
+++ client/src/index.js
@@ -1,25 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
-import rootReducer from './reducers/index';
-import logger from 'redux-logger';
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import rootReducer from "./reducers/index";
+import logger from "redux-logger";
 
-import './index.css';
-import './assets/scss/style.scss';
-import App from './App';
+import "./index.css";
+import App from "./App";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk, logger)));
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk, logger))
+);
 
 // // Set a same-site cookie for first-party contexts
 // document.cookie = "safeCookie1=foo; SameSite=Lax";
-// document.cookie = "safeCookie2=foo"; 
+// document.cookie = "safeCookie2=foo";
 // // Set a cross-site cookie for third-party contexts
 // document.cookie = "crossCookie=bar; SameSite=None; Secure";
-
 
 ReactDOM.render(
   <Provider store={store}>
@@ -27,9 +28,8 @@ ReactDOM.render(
       <App />
     </React.StrictMode>
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
