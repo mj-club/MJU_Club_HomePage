cd2ce0ea02ff833a964691f8ba14324d8881431f
JiWon <jiohning00@hanmail.net>
2021-07-23 03:06:12 +0900

    키움 이모저모 > 자유 게시판 글 작성하기/수정하기/조회 부분 생성, 메인 페이지 이미지 수정, 메인 페이지 폰트 수정

--- client/src/components/Router.js
+++ client/src/components/Router.js
@@ -13,9 +13,6 @@ import Page from "../components/Page";
 import JoinTermPage from "../pages/JoinTermPage"
 import WelcomePage from "../pages/WelcomePage"
 
-//키움 이모저모
-import About_Board from '../pages/about/BulletinBoardPage';
-
 // Pages 로 이동
 export default function Routers() {
   return(
@@ -30,7 +27,6 @@ export default function Routers() {
 
           <Route path="/promotion" component={PromotionPage}/>
           <Route path="/about" component={AboutPage}/>
-          <Route path="/bulletin-board" component={About_Board}/>
           <Route path="/monthly_keyum" component={MonthlyPage}/>
 
           <Route path="/login" component={LoginPage}/>
--- client/src/pages/AboutPage.js
+++ client/src/pages/AboutPage.js
@@ -1,20 +1,40 @@
 import React from 'react';
+import {
+  Switch,
+  Route,
+  useRouteMatch
+} from "react-router-dom";
+
 
 import Page from '../components/Page';
 import RightTitle from '../components/RightTitle';
 import Ipsum from '../components/Ipsum';
 
+import BoardPage from './about/bulletinBoard/ListPage';
+
 export default function AboutPage() {
-  const title="About"
+  let { path } = useRouteMatch();
+
+  const title="About";
   const rightTitle = <RightTitle 
             title={title}
             menu1={"HOME"}
-            menu2={"멤버십"}
-            menu3={title}
+            menu2={title}
+            menu3={"MAIN"}
           />
   const rightInner = <Ipsum title={title}></Ipsum>
 
   return(
-     <Page rightInner={rightInner} rightTitle={rightTitle}/>
+    <>
+      <Switch>
+        <Route exact path={path}>
+          <Page rightInner={rightInner} rightTitle={rightTitle}/>
+        </Route>
+        <Route path={`${path}/bulletin-board`}>
+          <BoardPage/>
+        </Route>
+      </Switch>
+    </>
+    
   );
 }
