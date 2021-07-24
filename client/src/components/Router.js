5c36b569021beff627ce3410a61873f36ff56aa6
jisu Yoon <jisu91113@gmail.com>
2021-07-24 15:41:07 +0900

    deleteAccountForm

--- client/src/components/Router.js
+++ client/src/components/Router.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-import LoginPage from '../pages/LoginPage';
-import JoinPage from '../pages/JoinPage';
+import LoginPage from '../pages/Auth/LoginPage';
+import JoinPage from '../pages/Auth/JoinPage';
+import FindIdPwPage from '../pages/Auth/FindIdPwPage';
+import JoinTermPage from "../pages/Auth/JoinTermPage";
+import WelcomePage from "../pages/Auth/WelcomePage"
 import MainPage from '../pages/MainPage';
-import FindIdPwPage from '../pages/FindIdPwPage';
 import MjuClubPage from '../pages/MjuClubPage';
 import PromotionPage from '../pages/PromotionPage';
 import AboutKeyUmPage from '../pages/AboutKeyUmPage';
 import MonthlyPage from '../pages/MonthlyPage';
 import Page from "../components/Page";
-import JoinTermPage from "../pages/JoinTermPage";
 import AboutClubUnion from "../pages/AboutClubUnion";
-import WelcomePage from "../pages/WelcomePage"
+import DeleteAccountPage from '../pages/Auth/DeleteAccountPage';
+
 
 // Pages 로 이동
 export default function Routers() {
@@ -35,6 +37,7 @@ export default function Routers() {
           <Route path="/join_term" component={JoinTermPage}/>
           <Route path="/welcome" component={WelcomePage}/>
           <Route path="/find_id_pw" component={FindIdPwPage}/>
+          <Route path="/delete_account" component={DeleteAccountPage} />
           <Route path="/page" component={Page}/>
           <Route path="/" exact={true}>
             <MainPage />
--- client/src/pages/Auth/JoinTermPage.js
+++ client/src/pages/Auth/JoinTermPage.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-import RightTitle from '../components/RightTitle';
-import JoinTerms from '../components/Auth/JoinTerms'
-import Page from '../components/Page';
+import RightTitle from '../../components/RightTitle';
+import JoinTerms from '../../components/Auth/JoinTerms'
+import Page from '../../components/Page';
 
 export default function JoinTermPage() {
   const title="회원 약관"
