f4e85df1b1d2065b3c8baf6520309e3ee4773d38
dj-1087 <sam1087@mju.ac.kr>
2021-08-02 22:10:04 +0900

    throw login error

--- server/passport/localStrategy.js
+++ server/passport/localStrategy.js
@@ -19,9 +19,13 @@ module.exports = () => {
             if (result) {
               done(null, exUser);
             } else {
+              var err = new Error("비밀번호가 일치하지 않습니다.");
+              err.name = "WrongPasswordError";
               done(null, false, { message: "비밀번호가 일치하지 않습니다." });
             }
           } else {
+            const err = new Error("가입되지 않은 회원입니다.");
+            err.name = "NoUserError";
             done(null, false, { message: "가입되지 않은 회원입니다." });
           }
         } catch (error) {
